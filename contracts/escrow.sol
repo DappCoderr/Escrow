9e8deff767c6933ee26571e1712515dccb0aafa7
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-01-24 23:35:55 +0530

    Update: Add release function

--- contracts/escrow.sol
+++ contracts/escrow.sol
@@ -1,18 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity ^0.8.20;
 
 contract Escrow{
     address public payer;
     address public receiver;
     address public arbiter;
-    State public state; 
+    bool public isApproved;
 
-    enum State{Created, Locked, Released, InDispute}
+    enum State{Created, Locked, Released, InDispute};
+    State public state;
 
-    constructor(address _receiver, address _arbiter){
+    emit Approved(uint);
+
+    constructor(address payable _receiver, address _arbiter){
         payer = msg.sender;
         receiver = _receiver;
         arbiter = _arbiter;
         state = State.Created;
+    }
+
+    function release() external payable{
+        require(msg.sender == arbiter);
+        uint balance = address(this).balance;
+        (bool sent, ) = receiver.call{value:balance}("");
+        require(sent, "Failed to send ether");
+        isApproved = true;
+        state = State.Released;
+        emit Approved(balance);
     }
 }
